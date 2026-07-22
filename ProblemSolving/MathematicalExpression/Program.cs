76f9fbda87d7a998222059f1185c9ce9fc764b03
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-22 22:02:42 +0300

    Solution of Mathematical Expression Problem

---
+++ ProblemSolving/MathematicalExpression/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MathematicalExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            string[] arr;
+            string[] arr2;
+            foreach (var e in expression)
+            {
+                if (e == '+')
+                {
+                    arr = expression.Split('+');
+                    arr2 = arr[1].Split("=");
+                    Console.WriteLine(((int.Parse(arr[0]) + int.Parse(arr2[0]))== int.Parse(arr2[1]))?"Yes": (int.Parse(arr[0]) + int.Parse(arr2[0])));
+                }
+                else if (e == '-')
+                {
+                    arr = expression.Split("-");
+                    arr2 = arr[1].Split("=");
+                    Console.WriteLine(((int.Parse(arr[0]) - int.Parse(arr2[0])) == int.Parse(arr2[1]))?"Yes": (int.Parse(arr[0]) - int.Parse(arr2[0])));
+                }
+                else if (e == '*')
+                {
+                    arr = expression.Split("*");
+                    arr2 = arr[1].Split("=");
+                    Console.WriteLine(((int.Parse(arr[0]) * int.Parse(arr2[0])) == int.Parse(arr2[1])) ? "Yes" : (int.Parse(arr[0]) * int.Parse(arr2[0])));
+                }
+                else if (e == '/')
+                {
+                    arr = expression.Split("/");
+                    arr2 = arr[1].Split("=");
+                    Console.WriteLine(((int.Parse(arr[0]) + int.Parse(arr2[0])) == int.Parse(arr2[1])) ? "Yes" : (int.Parse(arr[0]) + int.Parse(arr2[0])));
+                }
+            }
+        }
+    }
+}
