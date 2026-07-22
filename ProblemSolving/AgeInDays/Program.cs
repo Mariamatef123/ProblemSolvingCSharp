9693a80692b9b83c9bb767d6a03e6ab5520a36b5
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-22 20:15:13 +0300

    Solution of Age In Days Problem

---
+++ ProblemSolving/AgeInDays/Program.cs
@@ -0,0 +1,27 @@
+﻿//calc no of years , months,days
+namespace AgeInDays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int AgeInDays = int.Parse(Console.ReadLine());
+            int noOfYears = 0;
+            int noOfMonths = 0;
+            int noOfDays = 0;
+            while(AgeInDays >= 365)
+            {
+                noOfYears++;
+                AgeInDays -= 365;
+            }
+            while (AgeInDays >= 30) { 
+             noOfMonths++;
+                AgeInDays -= 30;
+            }
+            noOfDays = AgeInDays;
+            Console.WriteLine($"{noOfYears} years");
+            Console.WriteLine($"{noOfMonths} months");
+            Console.WriteLine($"{noOfDays} days");
+        }
+    }
+}
