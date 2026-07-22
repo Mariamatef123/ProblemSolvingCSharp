58449c800ffd2057cb864c4f81d6f0c1a4e13c44
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-22 23:46:48 +0300

    Solution of Min & Max ,theBrothers,welcone for you with condtions ,mutiples problems

---
+++ ProblemSolving/Max&Min/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Max_Min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+            int[] arr = numbers.Split(" ").Select(int.Parse).ToArray();
+            int min = arr[0];
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine($"{min} {max}");
+        }
+    }
+}
