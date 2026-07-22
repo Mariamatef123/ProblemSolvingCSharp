fb265db21c610c6c88b0c4d37519607854cc28a7
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 02:38:38 +0300

    Solution of Factorial Problem

---
+++ ProblemSolving/Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[]arr= new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i]=int.Parse(Console.ReadLine());
+            }
+           
+            for (int i = 0;i < n; i++)
+            {
+                int factorial = arr[i];
+                for (int j = arr[i]-1;j > 0 ; j--)
+                {
+                    factorial *= j;
+                }
+                Console.WriteLine(factorial);
+            }
+        }
+    }
+}
