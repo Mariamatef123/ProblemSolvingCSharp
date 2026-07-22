a459346e96b7268380e4180291e08b1d2135a6c4
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 02:08:26 +0300

    Solution of Even Numbers Problem

---
+++ ProblemSolving/EvenNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 != 0) continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
