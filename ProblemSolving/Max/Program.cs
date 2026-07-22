437f0c3db6c0df89350802b5e375c45f49aec1f1
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 02:25:19 +0300

    Solution of max problem

---
+++ ProblemSolving/Max/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Max
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string numbers = Console.ReadLine();
+            int[] nums = numbers.Split(" ").Select(int.Parse).ToArray();
+            if (nums.Length != n) {
+                Console.WriteLine("Wrong input. must be 5 numbers");
+                return;
+            }
+            int max = nums[0];
+            foreach(var num in nums)
+            {
+                if (num > max)
+                {
+                    max= num;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
