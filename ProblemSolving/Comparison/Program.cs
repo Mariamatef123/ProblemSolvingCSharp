e41c99043484553c1766e7070182ded629013b41
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-22 21:46:26 +0300

    Solution of Comparison Problem

---
+++ ProblemSolving/Comparison/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            string[] arr;
+            foreach (var e in expression)
+            {
+                if (e == '>')
+                {
+                    arr = expression.Split('>');
+                    Console.WriteLine((int.Parse(arr[0]) > int.Parse(arr[1]))?"Right":"Wrong");
+                }
+                else if (e == '<')
+                {
+                    arr = expression.Split("<");
+                    Console.WriteLine((int.Parse(arr[0]) < int.Parse(arr[1])) ? "Right" : "Wrong");
+                }
+                else if (e == '=')
+                {
+                    arr = expression.Split("=");
+                    Console.WriteLine((int.Parse(arr[0]) == int.Parse(arr[1])) ? "Right" : "Wrong");
+                }
+            }
+        }
+    }
+}
