da541b4a001a9abdb719c52ecab0fcb4593b2d59
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 22:22:09 +0300

    Solution of Easy Fibonacci Problem

---
+++ ProblemSolving/EasyFibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EasyFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] fib= new int[n];
+            for (int i = 1; i <= n; i++) {
+                if (i == 1) { fib[i - 1] = 0; }//fib(0)=0
+                else if (i == 2) { fib[i - 1] = 1; }//fib(1)=1
+                else
+                {
+                    fib[i - 1] = fib[i - 2] +fib[i - 3];
+                }
+            }
+            foreach (int i in fib) { 
+            Console.Write($"{i} ");
+            }
+        }
+    }
+}
