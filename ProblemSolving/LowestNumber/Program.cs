99e2bad533d6dd91d97011c563dd5ea454734c1f
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 23:35:01 +0300

    Solution of Lowest Number Problem

---
+++ ProblemSolving/LowestNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LowestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int index = 1;
+            int lowest = arr[0];
+            for (int i = 1; i < n; i++)
+            {
+                if( arr[i] < lowest)
+                {
+                    lowest= arr[i];
+                    index = i+1;
+                }
+            }
+            Console.WriteLine($"{lowest} {index}");
+
+        }
+    }
+}
