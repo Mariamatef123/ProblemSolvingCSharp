3c9a82970261bc88d3c7852f82634a6edd5da4df
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-24 00:23:52 +0300

    Solution of Smallest Pair Problem

---
+++ ProblemSolving/SmallestPair/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SmallestPair
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int noOfTestCases = int.Parse(Console.ReadLine());
+            int sum=0;
+            int smallest;
+            int[] result=new int[noOfTestCases];
+            for (int k = 0; k < noOfTestCases; k++)
+            {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+             sum= arr[0] + arr[1]+2-1;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (i < j || i != j)
+                        {
+                            smallest = arr[i] + arr[j] + (j + 1) - (i + 1);
+                            if (smallest < sum)
+                            {
+                                sum = smallest;
+                            }
+                        }
+                    }
+                }
+                result[k] = sum;
+            }
+            foreach(int i in result)
+            {
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
