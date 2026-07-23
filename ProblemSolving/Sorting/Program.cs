7d23066607e5e3ca875914bd388eac1b4712fbc6
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 23:38:47 +0300

    Solution of Sorting Problem

---
+++ ProblemSolving/Sorting/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int temp = 0;
+            for (int j = 0; j < n - 1; j++)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                    }
+                }
+            }
+            foreach (int i in arr) { 
+            Console.Write($"{i} ");
+            }
+
+        }
+    }
+}
