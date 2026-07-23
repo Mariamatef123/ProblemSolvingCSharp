91b0eab624f5ea305003c26ae82fbd7a3d8c0157
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 18:57:24 +0300

    Solution of Sequence Of Numbers & Sum problem

---
+++ ProblemSolving/SequenceOfNumbers&Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace SequenceOfNumbers_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> result = new List<string>();
+            string s;
+            string res;
+            int[] arr;
+            int sum;
+            int count=0;
+            while (true) {
+                s = Console.ReadLine();
+                arr = s.Split(" ").Select(int.Parse).ToArray();
+                if (arr.Contains(0) || arr.Any(x=>x<0)) break;
+                sum = 0;
+                res = null;
+                for (int i = Math.Min(arr[0], arr[1]); i <= Math.Max(arr[0], arr[1]); i++)
+                {
+                    if (res == null) { res = $"{i} "; }
+                    else { res += $"{i} "; }
+                        sum += i;
+                }
+                res+=$"sum ={sum}";
+                result.Insert(count ,res);
+                count++;
+            }
+
+            if (result != null)
+            {
+                foreach(string str in result)
+                {
+                        Console.WriteLine(str);
+                }
+            }
+        }
+
+    }
+}
