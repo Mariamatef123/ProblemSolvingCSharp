811d915ad1d323ff900205a80b8a9d77a73e0eb2
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 23:01:12 +0300

    Solution of Summation Problem

---
+++ ProblemSolving/Summation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Summation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int [] arr= Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();;
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                
+                sum += arr[i];
+            }
+            Console.WriteLine(Math.Abs(sum));
+        }
+    }
+}
