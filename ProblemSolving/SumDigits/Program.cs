4f51c27e35d06242d838b3f0dd56fdc845cefe65
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-24 02:28:36 +0300

    Solution of Sum Digits Problem

---
+++ ProblemSolving/SumDigits/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[] arr = int.Parse(Console.ReadLine()).ToString().ToCharArray();
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                sum += Convert.ToInt32(arr[i].ToString());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
