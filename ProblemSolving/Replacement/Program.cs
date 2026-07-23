3ee4f06ab121e1488335db4aef7ead9e1a018df2
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 23:09:59 +0300

    Solution of Replacement Problem

---
+++ ProblemSolving/Replacement/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Replacement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    arr[i] = 1;
+                }
+                else if (arr[i] < 0)
+                {
+                    arr[i] = 2;
+                }
+            }
+            foreach(int i in arr)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
