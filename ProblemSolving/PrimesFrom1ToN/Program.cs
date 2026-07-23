9f191b4658c6cd8dc24f1fb4b7704ab467bf59e6
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 15:51:21 +0300

    solution of Primes from 1 to N Problem

---
+++ ProblemSolving/PrimesFrom1ToN/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PrimesFrom1ToN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n < 2 || n > Math.Pow(10, 3)) return;
+            int[] notDivisable = [2, 3, 4, 5, 6];
+            int count = 0;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < notDivisable.Length; j++)
+                {
+                    if (i != notDivisable[j] && i!=1)
+                    {
+                        if (i % notDivisable[j] != 0)
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if (notDivisable.Contains(i))
+                {
+                    if (count == (notDivisable.Length) - 1)
+                    {
+                        Console.Write(i);
+                        Console.Write("\t");
+                    }
+                }
+                else
+                {
+                    if (count == (notDivisable.Length))
+                    {
+                        Console.Write(i);
+                        Console.Write("\t");
+                    }
+                }
+           count=0;
+            }
+        }
+    }
+}
