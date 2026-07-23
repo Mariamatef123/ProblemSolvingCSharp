5def76c0a54eac78bf1a35ca7f1906860afd85ab
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 22:39:51 +0300

    Solution of Three Numbers Problem

---
+++ ProblemSolving/ThreeNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          string numbers = Console.ReadLine();
+           int k =( numbers.Split(" ").Select(int.Parse).ToArray())[0];
+            int s = (numbers.Split(" ").Select(int.Parse).ToArray())[1];
+            int sum = 0;
+            for (int x = 0; x <= s; x++) {
+                for (int y = 0; y <= s; y++) { 
+                    for(int z = 0; z <= s; z++)
+                    {
+                        if (x <= k && y <= k && z <= k)
+                        {
+                            if (x + y + z == s)
+                            {
+                                sum++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
