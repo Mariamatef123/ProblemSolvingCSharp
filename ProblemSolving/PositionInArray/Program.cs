0120416707c12534d2078cd6ffda84457a727657
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-23 23:13:01 +0300

    Solution of Position In Array problem

---
+++ ProblemSolving/PositionInArray/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PositionInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < n; i++)
+            {
+                if(arr[i] <= 10)
+                {
+                    Console.WriteLine($"A[{i}] = {arr[i]}");
+                }
+            }
+        }
+    }
+}
