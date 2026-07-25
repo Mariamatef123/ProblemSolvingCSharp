839ca05dcc1e6ed81015154af5c58df779b740e3
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-25 03:14:00 +0300

    Solution of Fibonacci Problem

---
+++ ProblemSolving/Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr= new int[n];
+            for (int i = 1; i <= n; i++) { 
+                if(i==1) arr[i-1] = 0;
+                else if(i==2) arr[i-1] = 1;
+                else
+                {
+                    arr[i-1] = arr[i - 2] + arr[i-3]; 
+                }
+            }
+            Console.WriteLine(arr[n-1]);
+
+        }
+    }
+}
