f3141f5387ccd24408d1a4b3f59cda7f30d2029a
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-25 03:00:29 +0300

    Solution of Check Code ,Replace min and max Problems

---
+++ ProblemSolving/CheckCode/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CheckCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int a=numbers[0];
+            int b=numbers[1];
+            string s = Console.ReadLine();
+            if (s.ToString().Length != (a+b+1))
+            {
+                return;
+            }
+            int index = 0;
+            bool flag = false;
+            foreach (char c in s) { 
+                index++;
+            if(c == '-')
+                {
+                    if (index == (a + 1))
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else {
+                        Console.WriteLine("No");
+                    }
+                    flag = true;
+                    return;
+                }
+            }
+            if (!flag) {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ ProblemSolving/ReplaceMinMax/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ReplaceMinMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int min = arr[0];
+            int indexOfMin = 0;
+            int max = arr[0];
+            int indexOfMax = 0;
+            for (int i = 1; i < arr.Length; i++) {
+                if (arr[i] < min) { 
+                min = arr[i];
+                indexOfMin = i;
+                }
+                if (arr[i] > max) { 
+                max = arr[i];
+                indexOfMax = i;
+                }
+            }
+            arr[indexOfMax]=min; 
+            arr[indexOfMin]=max;
+            foreach (int i in arr) { 
+            Console.Write($"{i} ");
+            }
+        }
+    }
+}
